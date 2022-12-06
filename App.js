c13b6cc723fd3df53441a86692ce00729ae2614b
Steve-Hsu <wuujdsc@gmail.com>
2022-12-06 16:28:55 +0700

    Navigation between 2 pages

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 
 import { StyleSheet, StatusBar} from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator()
+
+
 
 export default function App() {
   return (
     <>
-        <StatusBar style='light'/>
-        <CategoriesScreen />
+        <StatusBar barStyle={'dark-content'}/>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="MealsCategories"  component={CategoriesScreen} />
+            <Stack.Screen name="MealsOverview"  component={MealsOverviewScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
     </>
 
   );
 }
 
 const styles = StyleSheet.create({
-  // padding: 50
 });
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,14 +3,18 @@ import CategoryGridTile from '../components/CategoryGridTile'
 import { CATEGORIES } from '../data/dummy-data';
 
 
-function renderCategoryItem(data){
-  return <CategoryGridTile 
-    title={data.item.title} 
-    color={data.item.color}
-    />
-}
+function CategoriesScreen({navigation}){
+  function renderCategoryItem(data){
+    function pressHandler(){
+      navigation.navigate("MealsOverview");
+    } 
+    return <CategoryGridTile 
+      title={data.item.title} 
+      color={data.item.color}
+      onPress={pressHandler}
+      />
+  }
 
-function CategoriesScreen(){
   return (<FlatList 
     data={CATEGORIES} 
     keyExtractor={(item)=>item.id} 
